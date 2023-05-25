449042b34268b3992c0591032ad04529cfed9c86
sabler <sabler@users.noreply.github.com>
2023-05-24 19:38:02 -0700

    Leading zeroes

--- clocker.js
+++ clocker.js
@@ -2,9 +2,9 @@ function testClock() {
     let clockInit = new Date();
     let localTime = clockInit.toLocaleTimeString();
 
-    let hours = clockInit.getHours();
-    let minutes = clockInit.getMinutes();
-    let seconds = clockInit.getSeconds();
+    let hours = clockInit.getHours() < 10 ? `0${clockInit.getHours()}` : clockInit.getHours();
+    let minutes = clockInit.getMinutes() < 10 ? `0${clockInit.getMinutes()}` : clockInit.getHours();
+    let seconds = clockInit.getSeconds() < 10 ? `0${clockInit.getSeconds()}`: clockInit.getSeconds();
 
 
     process.stdout.write(`${hours.toString()}:${minutes.toString()}:${seconds.toString()} \r`);
@@ -17,11 +17,5 @@ function testClock() {
     }
 
 }
-
-
-const {hours, minutes, seconds} = testClock();
-
-
-
 
 testInterval = setInterval(testClock,1000);
